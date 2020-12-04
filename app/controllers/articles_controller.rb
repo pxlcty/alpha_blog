380cfbf7274f4b4e7cfe4d1406425474701ffb1e
Michael McCracken <pxlcty@gmail.com>
2020-12-04 00:48:19 -0500

    Create form to add new articles: Add routes create & new, their actions and new.html.erb file. Use form_with helper to create secure form.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,23 @@
 class ArticlesController < ApplicationController
 
+    def index
+        @articles = Article.all
+    end
+
     def show
         #byebug
         @article = Article.find(params[:id])
     end
 
-    def index
-        @articles = Article.all
+    def create
+        # in the Rails S output we can see the Parameter hash.
+        # in that hash, we can see one of the keys are "article" hence we pull it out below to display basic output.
+        render plain: params[:article]
     end
+
+    def new
+    end
+
+
 
 end
