2e86f5664a1bd9a23d7c2d45c450d51efcc3e775
Michael McCracken <pxlcty@gmail.com>
2020-12-04 01:48:07 -0500

    Add save fucntionality to def create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,19 @@ class ArticlesController < ApplicationController
     def create
         # in the Rails S output we can see the Parameter hash.
         # in that hash, we can see one of the keys are "article" hence we pull it out below to display basic output.
-        render plain: params[:article]
+        # render plain: params[:article]
+        # with that :article key we can use that to create to the DB : 
+        # @article = Article.new(params[:article]) # won't work
+        # whitelist the :title & description data in the article hash:
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        #render plain: @article.inspect
+        @article.save
+        # url is created by 'article_path' => articles (Routes files for articles#show is /articles/:id)
+        # and the @article will pull out the :id to create articles/:id
+        #redirect_to article_path(@article)
+        #short hand for above is this : 
+        redirect_to @article
+
     end
 
     def new
