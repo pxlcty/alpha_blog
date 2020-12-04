2ede0cd248aa0565db80783155e5d1782307cd70
Michael McCracken <pxlcty@gmail.com>
2020-12-03 22:54:01 -0500

    Create Article index page. Add route, controller def and view with each loop of dynamic content.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,12 @@
 class ArticlesController < ApplicationController
 
     def show
-        byebug
+        #byebug
         @article = Article.find(params[:id])
+    end
+
+    def index
+        @articles = Article.all
     end
 
 end
