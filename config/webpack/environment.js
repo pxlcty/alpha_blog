63caa0812d0686c1df8726cc5c9f27d8e5794a13
Michael McCracken <pxlcty@gmail.com>
2020-12-07 17:33:57 -0500

    Add Bootstrap libraries including jQuer and Popper.js. Create Navigation and footer. Design home (root) page.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+
+// added for bootstrap-js to work (Jquery & Popper): 
+const webpack = require("webpack")
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+}))
+// end of bootstrap-js installation
+
 module.exports = environment
