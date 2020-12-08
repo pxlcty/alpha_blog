3f018deb5391d5fdbb31174424ee47b8164e254a
Michael McCracken <pxlcty@gmail.com>
2020-12-07 21:51:51 -0500

    Add styling to homepage + CRUD pages, and navigation.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# prevents errors from generating additional div in return error code : 
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe
+end
