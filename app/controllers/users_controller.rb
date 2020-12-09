022eb6f613112a22b1f1e1e88730c8ebbb6704d8
Michael McCracken <pxlcty@gmail.com>
2020-12-08 23:40:54 -0500

    Add pagination function with will_paginate gem.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,13 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        @articles = @user.articles
+        #@articles = @user.articles
+        @articles = @user.articles.paginate(page: params[:page], per_page: 3)
+    end
+
+    def index
+        #@users = User.all
+        @users = User.paginate(page: params[:page], per_page: 3)
     end
 
     def new
@@ -17,7 +23,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params_ww)
             flash[:notice] = "Your account was successfully updated."
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
